8e38fdc90e2beb4a0beb1f51b9e3fa17445d47d0
Josh Harcombe <joshharc@gmail.com>
2023-11-28 23:20:31 +1100

    stage 3

--- src/header.rs
+++ src/header.rs
@@ -58,23 +58,20 @@ pub enum Class {
 
 impl Header {
     pub fn to_bytes(self) -> BytesMut {
-        fn write_u16(s: &mut [u8], val: u16) {
-            s[0] = (val >> 8) as u8;
-            s[1] = val as u8;
-        }
-
         let mut s = BytesMut::with_capacity(12);
-        write_u16(&mut s, self.packet_id);
-        s[2] = (self.qr_indicator as u8) << 7
-            | self.opcode << 3
-            | (self.authoritative_answer as u8) << 2
-            | (self.truncation as u8) << 1
-            | self.recursion_desired as u8;
-        s[3] = (self.recursion_available as u8) << 7 | self.reserved << 4 | self.response_code;
-        write_u16(&mut s[4..], self.qd_count);
-        write_u16(&mut s[6..], self.an_count);
-        write_u16(&mut s[8..], self.nscount);
-        write_u16(&mut s[10..], self.arcount);
+        s.put_u16(self.packet_id);
+        s.put_u8(
+            (self.qr_indicator as u8) << 7
+                | self.opcode << 3
+                | (self.authoritative_answer as u8) << 2
+                | (self.truncation as u8) << 1
+                | self.recursion_desired as u8,
+        );
+        s.put_u8((self.recursion_available as u8) << 7 | self.reserved << 4 | self.response_code);
+        s.put_u16(self.qd_count);
+        s.put_u16(self.an_count);
+        s.put_u16(self.nscount);
+        s.put_u16(self.arcount);
 
         s
     }
